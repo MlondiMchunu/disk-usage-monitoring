4e2f23da54593784a14eef4fc9363ac324336535
MlondiMchunu <mlondiemchunu1@gmail.com>
2025-06-01 02:21:39 +0200

    refactoring

--- disk-usage.go
+++ disk-usage.go
@@ -6,33 +6,35 @@ import (
 	"syscall"
 )
 
-func getDiskUsage(path string){
-	var stat syscall.Staffs_t
-	err := syscall.Staffs_t(path, &stat)
-	if err != nil{
-		fmt.Println("Error fetching disk usage:",err)
+func getDiskUsage(path string) {
+	var stat syscall.Statfs_t
+	err := syscall.Statfs(path, &stat)
+	if err != nil {
+		fmt.Println("Error fetching disk usage:", err)
 		return
 	}
 	total := stat.Blocks * uint64(stat.Bsize)
 	free := stat.Bfree * uint64(stat.Bsize)
 	used := total - free
-	percentUsed := float64(used)/ float64(total) * 100
-	fmt.Printf("Disk usage of %s:\n",path)
-	fmt.printf("Total: %d GB\n",total/1e9)
-	fmt.printf("Free: %d GB\n", free/1e9)
+	percentUsed := float64(used) / float64(total) * 100
+	fmt.Printf("Disk usage of %s:\n", path)
+	fmt.Printf("Total: %d GB\n", total/1e9)
+	fmt.Printf("Free: %d GB\n", free/1e9)
+	fmt.Printf("Used: %d GB (%.2f%%)\n", used/1e9, percentUsed)
 }
-func main(){
+
+func main() {
 	path := "/"
 	if len(os.Args) > 1 {
 		path = os.Args[1]
 	}
 	_, err := os.Stat(path)
-	if os.IsNotExist(err){
-		fmt.Printf("Error: '%s' Path doesn't exist.\n",path)
+	if os.IsNotExist(err) {
+		fmt.Printf("Error: '%s' Path doesn't exist.\n", path)
 		return
-	}else if err != nil{
-		fmt.Printf("Error occued while accessing path %s: %v \n",path,err)
-		return 
+	} else if err != nil {
+		fmt.Printf("Error occurred while accessing path %s: %v\n", path, err)
+		return
 	}
 	getDiskUsage(path)
 }
